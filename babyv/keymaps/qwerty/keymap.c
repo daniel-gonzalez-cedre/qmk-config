3797c57196bceacb6489713979de7bf8e70b453a
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2021-03-01 13:15:15 -0500

    modified:   hex/babyv_colemak.hex 	modified:   hex/babyv_qwerty.hex 	modified:   keymaps/colemak/keymap.c 	modified:   keymaps/qwerty/keymap.c

--- babyv/keymaps/qwerty/keymap.c
+++ babyv/keymaps/qwerty/keymap.c
@@ -21,7 +21,6 @@ enum layer_names {
     BASE,
     LOWER,
     RAISE,
-    NUM,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -29,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_BSPC,
         KC_LCTRL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,           KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
         KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
-        KC_CAPS,            KC_LALT,    MO(RAISE),  KC_LGUI,        KC_SPC, MO(LOWER),  MO(RAISE),                  KC_ENT
+        KC_CAPS,            KC_LALT,    MO(RAISE),  KC_LGUI,        KC_SPC, MO(LOWER),  KC_TILDE,                   KC_ENT
     ),
     [LOWER] = LAYOUT_1u(
         KC_ESC,     KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,            KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_EQL,     KC_BSLS,    KC_DEL,
@@ -38,12 +37,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,                KC_TRNS,    KC_TRNS,    KC_TRNS,                        KC_SPC, KC_TRNS,   KC_TRNS,                                 KC_ENT
     ),
     [RAISE] = LAYOUT_1u(
-        KC_VOLU,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_7,   KC_8,   KC_9,   KC_NO,  KC_NO,
-        KC_VOLD,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_4,   KC_5,   KC_6,   KC_NO,  KC_NO,
-        KC_MUTE,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_1,   KC_2,   KC_3,   KC_NO,  KC_NO,
-        RESET,          KC_TRNS,    KC_TRNS,    KC_TRNS,            KC_SPC, KC_0,   KC_DOT,                 KC_ENT
-    ),
-    [NUM] = LAYOUT_1u(
         KC_VOLU,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_7,   KC_8,   KC_9,   KC_NO,  KC_NO,
         KC_VOLD,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_4,   KC_5,   KC_6,   KC_NO,  KC_NO,
         KC_MUTE,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,  KC_1,   KC_2,   KC_3,   KC_NO,  KC_NO,
